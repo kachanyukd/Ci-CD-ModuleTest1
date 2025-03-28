aaff307ec2866e955f91a00f0ceb575ffcdb9b55
kachanyukd <kachanyukdima@gmail.com>
2025-03-28 15:36:35 +0200

    adding printing function

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ def read_and_sort_population_data(file_name):
                 population = int(parts[2].strip())
                 countries.append((name, area, population))
 
-    # Sorting by area
     sorted_by_area = sorted(countries, key=lambda x: x[1])
 
-    # Sorting by population
     sorted_by_population = sorted(countries, key=lambda x: x[2])
 
     return sorted_by_area, sorted_by_population
+
+def print_sorted_data(sorted_by_area, sorted_by_population):
+    print("Сортування за площею:")
+    for country in sorted_by_area:
+        print(f"{country[0]}: Площа = {country[1]} кв. км, Населення = {country[2]} осіб")
+
+    print("\nСортування за населенням:")
+    for country in sorted_by_population:
+        print(f"{country[0]}: Площа = {country[1]} кв. км, Населення = {country[2]} осіб")
